83e5e8d45d6bf15850a61b090c3b7eafea278ba0
PhilipDocs <philip.cts@gmail.com>
2018-12-22 06:34:12 +0530

    Add files via upload

--- pca.r
+++ pca.r
@@ -20,3 +20,31 @@ colnames(train.data)[1]<-"mpg"
 model2<-lm(mpg~.,data=train.data)
 summary(model2)
 summary(model1)
+
+##########Same data apply factor analsysi
+n.factors <- 2   
+
+fit <- factanal(mtcars, 
+                n.factors,                # number of factors to extract
+                scores=c("regression"),
+                rotation="none")
+
+print(fit, digits=2, cutoff=.3, sort=TRUE)
+load <- fit$loadings[,1:2] 
+plot(load,type="n") # set up plot 
+text(load,labels=names(mtcars),cex=.7) # add variable names
+#########Another method
+fit <- factanal(mtcars, 
+                n.factors,              # number of factors to extract
+                rotation="varimax")     # 'varimax' is an ortho rotation
+
+load <- fit$loadings[,1:2] 
+load
+library(psy)
+scree.plot(fit$correlation)
+library(nFactors)
+ev <- eigen(cor(mtcars)) # get eigenvalues
+ap <- parallel(subject=nrow(mtcars),var=ncol(mtcars), rep=100, cent=.05)
+nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+plotnScree(nS)
+####################################
